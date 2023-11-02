8331509969dca0a2652ff57c9a1aa269f5c2c6ee
LeonardoNatale <natale.leonardo1997@gmail.com>
2023-11-02 22:26:52 +0100

    move llm_connector

---
+++ app/services/llm_service.py
@@ -0,0 +1,19 @@
+import modal
+
+
+class LLMService:
+    pass
+
+
+stub = modal.Stub("example-get-started")
+
+
+@stub.function()
+def square(x):
+    print("This code is running on a remote worker!")
+    return x**2
+
+
+@stub.local_entrypoint()
+def main():
+    print("the square is", square.remote(42))
